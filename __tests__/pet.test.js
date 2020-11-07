0b1a4a503a438bbefd6b16a09584caf50f11df8d
Paul Hardy <pauljosephhardy@gmail.com>
2020-11-07 12:39:58 +0000

    update 2 7th Nov

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,17 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns a pet name', () => {
+    it('returns a pet name, inital age of 0', () => {
       const pet = new Pet('Dave')
       expect(pet.name).toBe('Dave');
+      expect(pet.age).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Dave');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
     });
   });
