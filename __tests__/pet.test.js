8f1c662b82bc80eb3d6b17f45e708bfc6f730a20
Paul Hardy <pauljosephhardy@gmail.com>
2020-11-07 19:44:30 +0000

    update 5 7th Nov

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -28,3 +28,50 @@ describe('growUp', () => {
       expect(pet.fitness).toEqual(10);
     });
   });
+
+  describe('feed', () => {
+    it('Decreases fitness by 3', () => {
+      const pet = new Pet('Dave');
+      pet.hunger = 2;
+      pet.feed();
+      expect(pet.hunger).toEqual(0);
+    });
+  });
+
+  describe('feel great', () => {
+    it('I feel great!', () => {
+      const pet = new Pet('Dave');
+      pet.fitness = 4;
+      pet.hunger = 4;
+      pet.feeling();
+      expect(pet.status).toEqual('I feel great!');
+    });
+  });
+
+  describe('need a walk', () => {
+    it('I need a walk', () => {
+      const pet = new Pet('Dave');
+      pet.fitness = 3;
+      pet.feeling();
+      expect(pet.status).toEqual('I need a walk');
+    });
+  });
+
+  describe('hungry', () => {
+    it('I am hungry', () => {
+      const pet = new Pet('Dave');
+      pet.hunger = 5;
+      pet.feeling();
+      expect(pet.status).toEqual('I am hungry');
+    });
+  });
+
+  describe('hungry and need a walk', () => {
+    it('I am hungry AND I need a walk', () => {
+      const pet = new Pet('Dave');
+      pet.fitness = 2;
+      pet.hunger = 6;
+      pet.feeling();
+      expect(pet.status).toEqual('I am hungry AND I need a walk');
+    });
+  });
--- src/pet.js
+++ src/pet.js
@@ -4,13 +4,16 @@ const INITIAL_AGE = 0;
 const INITIAL_HUNGER = 0;
 const GROWTH_AGE = 1;
 const GROWTH_HUNGER = 5;
-const GROWTH_FITNESS = 3;  
+const GROWTH_FITNESS = 3;
+const FEED_HUNGER = 3;
+const MINIMUM_HUNGER = 0;  
 
 function Pet(name) {
     this.name = name;
     this.age = INITIAL_AGE;
     this.hunger = INITIAL_HUNGER;
     this.fitness = MAXIMUM_FITNESS;
+    this.status = '';
 }
 
 Pet.prototype.growUp = function() {
@@ -23,8 +26,29 @@ Pet.prototype.walk = function() {
     if ((this.fitness + WALK_FITNESS) <= MAXIMUM_FITNESS ) {
         this.fitness += WALK_FITNESS;
     } else {
-        this.fitness = MAXIMUM_FITNESS
+        this.fitness = MAXIMUM_FITNESS;
     }
 }
+
+Pet.prototype.feed = function() {
+    if ((this.hunger - FEED_HUNGER) >= MINIMUM_HUNGER ) {
+        this.hunger -= FEED_HUNGER;
+    } else {
+        this.hunger = MINIMUM_HUNGER;
+    }
+}
+
+Pet.prototype.feeling = function() {
+    if (this.fitness > GROWTH_FITNESS && this.hunger < GROWTH_HUNGER) {
+        this.status = 'I feel great!';
+    } else if (this.fitness <= GROWTH_FITNESS && this.hunger >= GROWTH_HUNGER) {
+        this.status = 'I am hungry AND I need a walk';
+    } else if (this.fitness <= GROWTH_FITNESS) {
+        this.status = 'I need a walk';
+    } else { (this.hunger >= GROWTH_HUNGER)
+        this.status = 'I am hungry';
+    }
+
+} 
 
 module.exports = Pet;
